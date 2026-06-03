73594cedb15788000b822a54ff21da64e711b747
Dior Shelton <dior.shelton@gmail.com>
2026-06-02 23:55:27 -0400

    feat(handlers): Add handler methods and register routes

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -19,6 +19,9 @@ func main() {
 	store := cache.New()
 
 	router.Register("GET", "/keys", handlers.HandleGetAll(store))
+	router.Register("GET", "/keys/:id", handlers.HandleGet(store))
+	router.Register("PUT", "/keys/:id", handlers.HandlePut(store))
+	router.Register("DELETE", "/keys/:id", handlers.HandleDelete(store))
 
 	server.Run(*portPtr, *workersPtr, &router)
 }
--- handlers/keys.go
+++ handlers/keys.go
@@ -21,3 +21,60 @@ func HandleGetAll(store *cache.Store) func(*server.Request) *server.Response {
 	}
 
 }
+
+func HandleGet(store *cache.Store) func(*server.Request) *server.Response {
+	return func(r *server.Request) *server.Response {
+
+		if r.Params["id"] == "" {
+			return &server.Response{StatusCode: 400, Body: []byte("Bad Request")}
+		}
+
+		keyVal, err := store.Get(r.Params["id"])
+		if err != nil {
+			response := err.Error()
+			return &server.Response{StatusCode: 404, Body: []byte(response)}
+		}
+
+		return &server.Response{StatusCode: 200, Body: keyVal}
+
+	}
+
+}
+
+func HandlePut(store *cache.Store) func(*server.Request) *server.Response {
+	return func(r *server.Request) *server.Response {
+
+		if r.Params["id"] == "" || len(r.Body) == 0 {
+			return &server.Response{StatusCode: 400, Body: []byte("invalid/empty body")}
+		}
+
+		ok := json.Valid(r.Body)
+
+		if !ok {
+			return &server.Response{StatusCode: 400, Body: []byte("invalid")}
+		}
+
+		val := r.Body
+
+		store.Set(r.Params["id"], val)
+
+		return &server.Response{StatusCode: 201, Body: val}
+	}
+}
+
+func HandleDelete(store *cache.Store) func(*server.Request) *server.Response {
+	return func(r *server.Request) *server.Response {
+
+		if r.Params["id"] == "" {
+			return &server.Response{StatusCode: 400, Body: []byte("Bad Request")}
+		}
+
+		val, err := store.Delete(r.Params["id"])
+		if err != nil {
+			response := err.Error()
+			return &server.Response{StatusCode: 404, Body: []byte(response)}
+		}
+
+		return &server.Response{StatusCode: 200, Body: val}
+	}
+}
